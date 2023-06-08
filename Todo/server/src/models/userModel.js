b7a434cc031e97773db23e79dccef1d6f426f336
AhsanDev404 <ahsan.dev.404@gmail.com>
2023-06-08 20:37:39 +0500

    .

--- Todo/server/src/models/userModel.js
+++ Todo/server/src/models/userModel.js
@@ -43,7 +43,7 @@ userSchema.pre("save", async function (next) {
 });
 userSchema.methods.getJwtToken = function(){
     return jwt.sign({id:this._id},process.env.JWT_SECRET,{
-      expiresIn:"518400000"
+      expiresIn:"2h"
     })
 }
 userSchema.methods.comparePassword = async function(enteredPassword){
--- Todo/web/vite.config.js
+++ Todo/web/vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
+  server: {
+    middleware: [
+      createProxyMiddleware('/api', {
+        target: 'http://localhost:4000', // Replace with your API server URL
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''), // Remove the '/api' prefix
+      }),
+    ],
+  },
 })
